205d70a3ab5411ce799ff57ffcb518e3e356f3cd
Abdul Wahab Butt <abdulwahab9811@gmail.com>
2024-02-14 17:16:25 -0500

    usercontroller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,8 +5,42 @@ import asyncHandler from 'express-async-handler';
 //@acces public 
 
 const authUser = asyncHandler (async(req,res) => {
-    res.status(401);
-    throw error ('Something went wrong' , 'Authentication failed');
+    res.status(200).json({message:'Auth user' });
+
+});
+
+//@desc  Auth user/set token
+//route POST api/users/auth
+//@acces public 
+
+const authUser = asyncHandler (async(req,res) => {
+    res.status(200).json({message:'Auth user' });
+
+});
+
+//@desc  Auth user/set token
+//route POST api/users/auth
+//@acces public 
+
+const authUser = asyncHandler (async(req,res) => {
+    res.status(200).json({message:'Auth user' });
+
+});
+
+//@desc  Auth user/set token
+//route POST api/users/auth
+//@acces public 
+
+const authUser = asyncHandler (async(req,res) => {
+    res.status(200).json({message:'Auth user' });
+
+});
+
+//@desc  Auth user/set token
+//route POST api/users/auth
+//@acces public 
+
+const authUser = asyncHandler (async(req,res) => {
     res.status(200).json({message:'Auth user' });
 
 });
