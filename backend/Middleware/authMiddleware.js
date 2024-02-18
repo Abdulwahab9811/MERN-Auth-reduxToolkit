abbb41a775f29c1df08e384067063ce5d2e8db3a
Abdul Wahab Butt <abdulwahab9811@gmail.com>
2024-02-17 19:54:16 -0500

    autherror

--- backend/Middleware/authMiddleware.js
+++ backend/Middleware/authMiddleware.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 import asyncHandler from "express-async-handler";
-import User from "../models/User.js";
+import User from "../models/userModels.js";
 
 
 const protect = asyncHandler(async(req,res,next) => {
