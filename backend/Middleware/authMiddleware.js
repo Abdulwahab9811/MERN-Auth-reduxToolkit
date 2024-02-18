c3f77625bc5d798f2749047dd3c7b5702606fc1b
Abdul Wahab Butt <abdulwahab9811@gmail.com>
2024-02-18 18:34:21 -0500

    vite.js

--- backend/Middleware/authMiddleware.js
+++ backend/Middleware/authMiddleware.js
@@ -22,7 +22,7 @@ const protect = asyncHandler(async(req,res,next) => {
         
     }else {
         res.status(401);
-        throw new Error ('Not Authorized, invalid token');
+        throw new Error ('Not Authorized, no token');
 
 
     }
