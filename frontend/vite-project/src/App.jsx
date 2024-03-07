d66fade214047f60400761cc603209806f1ebab9
Abdul Wahab Butt <abdulwahab9811@gmail.com>
2024-03-07 13:57:54 -0500

    react-toastify

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { Container } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
+import ToastContainer from 'react-bootstrap';
 
 const App = () => {
   return (
