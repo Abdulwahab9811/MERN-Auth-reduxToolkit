f4369a1a29ac285f64819e1bb2b258487e14fe21
Abdul Wahab Butt <abdulwahab9811@gmail.com>
2024-03-07 13:47:21 -0500

    Loginscreen update

--- frontend/vite-project/src/screens/Loginscreen.jsx
+++ frontend/vite-project/src/screens/Loginscreen.jsx
@@ -6,80 +6,78 @@ import FormContainer from '../components/FormContainer';
 import { useLoginMutation} from '../slices/usersApiSlice'
 import { setCredentials } from "../slices/authSlices";
 
-import React from 'react'
-
-const Loginscreen = () => {
-
+const LoginScreen = () => {
     const [email, setEmail] = useState('');
-    const [password,SetPassword] = useState('');
-
-    const navigate = useNavigate();
+    const [password, setPassword] = useState('');
+  
     const dispatch = useDispatch();
-
-    const [login, {isLoading}] = useLoginMutation();
-
-    const {userInfo} = useSelector((state) => state.auth);
-
+    const navigate = useNavigate();
+  
+    const [login, { isLoading }] = useLoginMutation();
+  
+    const { userInfo } = useSelector((state) => state.auth);
+  
     useEffect(() => {
-        if (userInfo) {
-            navigate('/');
-        }
-    },[navigate,userInfo])
-
-    const sumbitHandler = async (e) => {
-        e.preventDefault();
-        try {
-            const res = await login({email,password}).unwrap();
-            dispatch(setCredentials({...res}))
-            navigate('/')
-            
-        } catch (err) {
-            console.log(err?.data.message ||err.error);
-            
-        }
-
-    }
-
-    
-  return (
-    <FormContainer>
-        <h1>Sign in</h1>
-        <Form onSumbit={sumbitHandler}>
-            <Form.Group className='my-2' controleId='email'>
-                <Form.Label>Email Adresse</Form.Label>
-                <Form.Control
-                type="email"
-                placeholder="email"
-                value={email}
-                onChange={(e)=> setEmail(e.target.value) }>
-
-                </Form.Control>
-            </Form.Group>
-
-            <Form.Group className='my-2' controleId='password'>
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                type="password"
-                placeholder="Enter Password"
-                value={password}
-                onChange={(e)=> SetPassword(e.target.value) }>
-
-                </Form.Control>
-            </Form.Group>
-
-            <Button type = 'Sumbit' variant='primary' className="mt-3">
-                Sign in
-            </Button>
-
-            <Row className="py-3">
-                <Col>
-                New Constumer? <Link to= '/register'>Register</Link>
-                </Col>
-            </Row>
+      if (userInfo) {
+        navigate('/');
+      }
+    }, [navigate, userInfo]);
+  
+    const submitHandler = async (e) => {
+      e.preventDefault();
+      try {
+        const res = await login({ email, password }).unwrap();
+        dispatch(setCredentials({ ...res }));
+        navigate('/');
+      } catch (err) {
+        console.log(err?.data?.message || err.error);
+      }
+    };
+  
+    return (
+      <FormContainer>
+        <h1>Sign In</h1>
+  
+        <Form onSubmit={submitHandler}>
+          <Form.Group className='my-2' controlId='email'>
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type='email'
+              placeholder='Enter email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+  
+          <Form.Group className='my-2' controlId='password'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type='password'
+              placeholder='Enter password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+  
+          <Button
+            disabled={isLoading}
+            type='submit'
+            variant='primary'
+            className='mt-3'
+          >
+            Sign In
+          </Button>
         </Form>
-    </FormContainer>
-  )
-}
-
-
-export default Loginscreen
+  
+        {isLoading && <p>Loading...</p>}
+  
+        <Row className='py-3'>
+          <Col>
+            New Customer? <Link to='/register'>Register</Link>
+          </Col>
+        </Row>
+      </FormContainer>
+    );
+  };
+  
+  export default LoginScreen;
