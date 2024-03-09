efa90067c2826b2d0f57733518c5336550e1d03e
Abdul Wahab Butt <abdulwahab9811@gmail.com>
2024-03-09 17:39:59 -0500

    toastify

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,12 +1,14 @@
 import { Container } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
-import ToastContainer from 'react-bootstrap';
+import { ToastContainer } from 'react-bootstrap';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <>
       <Header />
+      <ToastContainer/>
       <Container className='my-2'>
         <Outlet />
       </Container>
